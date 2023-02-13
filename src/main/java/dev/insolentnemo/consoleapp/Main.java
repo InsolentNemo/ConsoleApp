add1b31da7c85141ce25e8dbcf55d099bfb346fd
Nemo <72466575+InsolentNemo@users.noreply.github.com>
2023-02-13 23:36:34 +0100

    fix compile error

--- src/main/java/dev/insolentnemo/consoleapp/Main.java
+++ src/main/java/dev/insolentnemo/consoleapp/Main.java
@@ -7,12 +7,19 @@ import dev.insolentnemo.consoleapp.utils.Logger;
 public class Main {
     public static void main(String[] args) {
         new ConsoleApp() {
+
             @Override
             public void onStarted() {
                 final String title = ConsoleApp.class.getPackage().getImplementationTitle();
                 final String version = ConsoleApp.class.getPackage().getImplementationVersion();
                 Logger.println(ConsoleColor.PURPLE_BRIGHT + title + " v" + version);
             }
+
+            @Override
+            public void addCommands() {
+
+            }
+
         }.start();
 
     }
