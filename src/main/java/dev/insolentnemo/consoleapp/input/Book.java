b766af1750cc71a6997ea0044eb543f4bb6a44e1
Nemo <72466575+InsolentNemo@users.noreply.github.com>
2023-02-13 22:43:05 +0100

    create Book class for dynamical page systems

---
+++ src/main/java/dev/insolentnemo/consoleapp/input/Book.java
@@ -0,0 +1,77 @@
+package dev.insolentnemo.consoleapp.input;
+
+import dev.insolentnemo.consoleapp.utils.Logger;
+
+import java.util.List;
+
+public abstract class Book extends Command {
+
+    private short linesPerPage = 10;
+    private List<String> list;
+
+    public Book(String usage) {
+        super(usage);
+    }
+
+    @Override
+    public void onCommand(String[] args) {
+        list = onListRefresh();
+        final int maxPages = (int) Math.ceil((double) list.size() / linesPerPage);
+        final int page;
+
+        if (args.length == 0) page = 1;
+        else {
+            try {
+                page = Integer.parseInt(args[0]);
+            } catch (NumberFormatException exception) {
+                Logger.argumentError(args[0]);
+                return;
+            }
+        }
+
+        final String pageStr = getPage(page);
+
+        if (pageStr == null) {
+            Logger.error("Page '" + page + "' is invalid. Available pages: 1-" + maxPages);
+            return;
+        }
+
+        Logger.println(pageStr);
+    }
+
+    @Override
+    public void registerSubCommands() {
+
+    }
+
+    public abstract List<String> onListRefresh();
+
+    private String getPage(int page) {
+        final int maxPages = (int) Math.ceil((double) list.size() / linesPerPage);
+
+        if (page > maxPages || page < 1) return null;
+
+        final StringBuilder pageStr = new StringBuilder("Page " + page + "/" + maxPages + ":");
+
+        for (int i = 0; i < linesPerPage; i++) {
+            final int index = (linesPerPage * (page - 1)) + i;
+
+            if (index > list.size() - 1) break;
+
+            final String line = list.get(index);
+            pageStr.append("\n");
+            pageStr.append(line);
+        }
+
+        return pageStr.toString();
+    }
+
+    public short getLinesPerPage() {
+        return linesPerPage;
+    }
+
+    public void setLinesPerPage(short linesPerPage) {
+        this.linesPerPage = linesPerPage;
+    }
+
+}
--- src/main/java/dev/insolentnemo/consoleapp/input/commands/HelpCommand.java
+++ src/main/java/dev/insolentnemo/consoleapp/input/commands/HelpCommand.java
@@ -1,69 +1,28 @@
 package dev.insolentnemo.consoleapp.input.commands;
 
+import dev.insolentnemo.consoleapp.input.Book;
 import dev.insolentnemo.consoleapp.input.Command;
 import dev.insolentnemo.consoleapp.utils.CommandHandler;
-import dev.insolentnemo.consoleapp.utils.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class HelpCommand extends Command {
-
-    private final int maxLinesPerPage = 10;
+public class HelpCommand extends Book {
 
     public HelpCommand() {
         super("help <page>");
     }
 
     @Override
-    public void onCommand(String[] args) {
+    public List<String> onListRefresh() {
         final List<Command> commands = CommandHandler.getCommands();
-        final int maxPages = (int) Math.ceil((double) commands.size() / maxLinesPerPage);
-        final int page;
+        final List<String> usageList = new ArrayList<>();
 
-        if (args.length == 0) page = 1;
-        else {
-            try {
-                page = Integer.parseInt(args[0]);
-            } catch (NumberFormatException exception) {
-                Logger.argumentError(args[0]);
-                return;
-            }
+        for (Command command : commands) {
+            final String usage = command.getUsage();
+            usageList.add(usage);
         }
 
-        final String pageString = getPage(page);
-
-        if (pageString == null) {
-            Logger.error("Page '" + page + "' is invalid. Available pages: 1-" + maxPages);
-            return;
-        }
-
-        Logger.println(pageString);
+        return usageList;
     }
-
-    @Override
-    public void registerSubCommands() {
-
-    }
-
-    private String getPage(int page) {
-        final List<Command> commands = CommandHandler.getCommands();
-        final int maxPages = (int) Math.ceil((double) commands.size() / maxLinesPerPage);
-
-        if (page > maxPages || page < 1) return null;
-
-        final StringBuilder pageMessage = new StringBuilder("Commands page " + page + "/" + maxPages + ":");
-
-        for (int i = 0; i < maxLinesPerPage; i++) {
-            final int index = (maxLinesPerPage * (page - 1)) + i;
-
-            if (index > commands.size() - 1) break;
-
-            final Command command = commands.get(index);
-            pageMessage.append("\n");
-            pageMessage.append(command.getUsage());
-        }
-
-        return pageMessage.toString();
-    }
-
 }
